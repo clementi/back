fd16989262de21ec8c6f61b46472f33633e591ee
Jeffrey Pratt <clementi@users.noreply.github.com>
2021-01-18 16:26:06 -0700

    Extract println function

--- src/main/java/com/tenfactorial/back/handlers/WordHandler.java
+++ src/main/java/com/tenfactorial/back/handlers/WordHandler.java
@@ -1,5 +1,7 @@
 package com.tenfactorial.back.handlers;
 
+import com.tenfactorial.back.functions.UnaryStackFunction;
+
 import java.util.LinkedList;
 import java.util.Set;
 
@@ -15,6 +17,8 @@ public interface WordHandler {
             s.push(x);
             s.push(x);
         });
+
+        UnaryStackFunction printlnFunction = (s, x) -> System.out.println(x);
 
         head.attach(new UnaryOperationHandler("DROP", "Dropping", (s, x) -> {}))
                 .attach(new BinaryOperationHandler("SWAP", "Swapping", (s, x, y) -> {
@@ -36,9 +40,9 @@ public interface WordHandler {
                     s.push(y / x);
                 }))
                 .attach((new UnaryOperationHandler("PRINT", "Printing", (s, x) -> System.out.print(x))))
-                .attach((new UnaryOperationHandler("PRINTLN", "Printing with a newline", (s, x) -> System.out.println(x))))
+                .attach((new UnaryOperationHandler("PRINTLN", "Printing with a newline", printlnFunction)))
+                .attach((new UnaryOperationHandler(".", "Printing with a newline", printlnFunction)))
                 .attach((new NullaryOperationHandler("CLEAR", LinkedList::clear)))
-                .attach((new NullaryOperationHandler(".", (s) -> System.out.println(s.pop()))))
                 .attach(new UnknownOperationHandler((word) -> {
                     throw new IllegalArgumentException(String.format("Unknown word '%s'", word));
                 }));
