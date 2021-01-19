65c107e882ed8d2571fdf62c1c2f47e1e9f28df0
Jeffrey Pratt <clementi@users.noreply.github.com>
2021-01-18 22:12:31 -0700

    Put version in properties file; print version in REPL

--- src/main/java/com/tenfactorial/back/Repl.java
+++ src/main/java/com/tenfactorial/back/Repl.java
@@ -3,15 +3,28 @@ package com.tenfactorial.back;
 import org.jetbrains.annotations.NotNull;
 
 import java.io.BufferedReader;
+import java.io.FileNotFoundException;
 import java.io.IOException;
 import java.io.InputStreamReader;
 import java.util.Collections;
 import java.util.LinkedList;
 import java.util.List;
+import java.util.Properties;
 
 public class Repl {
     public void run() throws IOException {
-        System.out.println("Back interpreter");
+        var properties = new Properties();
+        var propertiesFileName = "config.properties";
+
+        var inputStream = getClass().getClassLoader().getResourceAsStream(propertiesFileName);
+
+        if (inputStream != null) {
+            properties.load(inputStream);
+        } else {
+            throw new FileNotFoundException("properties not found");
+        }
+
+        System.out.printf("Back %s%n", properties.getProperty("version"));
         System.out.println("Enter :quit or :q to exit");
 
         var reader = new BufferedReader(new InputStreamReader(System.in));
